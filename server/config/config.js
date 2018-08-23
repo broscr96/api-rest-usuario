c9b91e9f49a36f5594352c579ea77448f50b6ced
BrOsCR <broscr96@gmail.com>
2018-08-23 17:51:04 -0500

    api rest - usuario

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+//if (process.env.NODE_ENV === 'dev') {
+//   urlDB = 'mongodb://localhost:27017/cafe'
+//} else {
+urlDB = 'mongodb://broscr-user:VuddutnasEmyuk2@ds055564.mlab.com:55564/broscr'
+    //}
+process.env.URLDB = urlDB;
---
+++ server/server.js
@@ -0,0 +1,23 @@
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos online');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
